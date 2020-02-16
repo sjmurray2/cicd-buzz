c8641f13e6a43e7f07a7813bc02e0266fc1ab15d
sjmurray2 <sjmurray2@mail.mccneb.edu>
2020-02-16 22:58:48 +0000

    undo break

--- buzz/generator.py
+++ buzz/generator.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import random
 
-buzz = ('')
+buzz = ('continuous testing', 'continuous integration', 'continuous deployment', 'continuous improvement', 'devops')
 adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
 adverbs = ('remarkably', 'enormously', 'substantially', 'significantly', 'seriously')
 verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
